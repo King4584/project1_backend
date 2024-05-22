3d77a575d9e5616cf55c2c42631c09e560bc1c94
Utsav <pk0385599@gmail.com>
2024-05-22 16:20:32 +0530

    Added register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
-import express from 'express';
-import dotenv from 'dotenv';
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './env'
+})
 
-dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 8000;
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => { 
+        console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
-// Handle errors on the server
-server.on('error', (error) => {
-  if (error.code === 'EACCES') {
-    console.error(`Port ${port} requires elevated privileges`);
-  } else if (error.code === 'EADDRINUSE') {
-    console.error(`Port ${port} is already in use`);
-  } else {
-    console.error(`Error starting server: ${error.message}`);
-  }
-  process.exit(1); // Exit the process with an error code
-});
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,19 +1,19 @@
 class ApiError extends  Error{
-    Constructor(
+    constructor(
         statusCode,
         message="Something Went Wrong",
         errors=[],
         stack=""
     ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null,
-        this.message=message
+        super(message);
+        this.statusCode=statusCode;
+        this.data=null;
+        this.message=message;
         this.success=false;
-        this.errors=errors
+        this.errors=errors;
 
         if(stack){
-            this.stack=stack
+            this.stack=stack;
         }
         else{
             Error.captureStackTrace(this,this.Constructor)
